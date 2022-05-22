6abacdc9754f12cd733e350c7b2844a55e390979
NimaDT <94492664+NimaDT@users.noreply.github.com>
2022-05-22 16:17:41 +0200

    Add files via upload

---
+++ NPV_real_estate.py
@@ -0,0 +1,12 @@
+def NPV_real_estate(operating_net, required_rate_of_return, inflation):
+    """Takes operating net, required rate of return and inflation as inputs to calculate Net Present Value of a real estate investment.
+    Parameters:
+        operating_net (int or float): The net operating income of a property
+        required_rate_of_return (float): The required rate of return as a decimal number. Ex: 2% -> 0.02
+        inflation (float): The expected rate of inflation as a decimal number. Ex:: 3.5% -> 0.035
+
+    Returns:
+    NPV (float): The Net Present Value, i.e. the appropiate investment for a property given the parameters.
+        """
+    NPV = operation_net * ((1 + inflation) / ((1 + required_rate_of_return) - (1 + inflation)))
+    return NPV
